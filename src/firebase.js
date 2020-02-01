242be6d131ad0f4117bd3eb38dda1b488f4150ab
Dakshraj Sharma <sharmarajdaksh@gmail.com>
2020-02-01 18:01:57 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const firebaseConfig = firebase.initializeApp({
-	apiKey: 'AIzaSyDCitiQ2Wkm6DylwEGumAtZV0thHLMxBOI',
-	authDomain: 'todoist-clone-a48f6.firebaseapp.com',
-	databaseURL: 'https://todoist-clone-a48f6.firebaseio.com',
-	projectId: 'todoist-clone-a48f6',
-	storageBucket: 'todoist-clone-a48f6.appspot.com',
-	messagingSenderId: '373259292021',
-	appId: '1:373259292021:web:a99a818919be616ce0573c'
+	apiKey: 'YOURAPIKEY',
+	authDomain: 'YOURDOMAIN',
+	databaseURL: 'YOURURL',
+	projectId: 'YOURPROJECTID',
+	storageBucket: 'STORAGEBUCKER',
+	messagingSenderId: 'ID',
+	appId: 'ID'
 });
 
 export { firebaseConfig as firebase };
